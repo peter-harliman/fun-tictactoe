32b7b9063bc0458c939b38f90e6e322cd6601295
Peter Harliman Liem <peter.harliman@gmail.com>
2016-11-13 00:09:03 +0800

    Add simple box drawing
    Signed-off-by: Peter Harliman Liem <peter.harliman@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,42 @@
+
+
 // Main function
 fn main() {
     println!("Enter");
-    init_box();
+
+    let mut my_box = [0; 9];
+    init_box(&mut my_box);
+    draw_box(my_box);
+
+    let mut turn = 0;
+    loop {
+
+    }
+
     println!("Exit");
 }
 
 // Initialize box
-fn init_box() {
+fn init_box(input_box: &mut [i32; 9]) {
     println!("Initializing box");
-    let my_box = [0; 9];
+    for i in 0..9 {
+        input_box[i] = 0;
+    }
     println!("Finished initializing");
+}
+
+// Draw box
+fn draw_box(input_box: [i32; 9]) {
+    println!("Draw box");
+    for i in 0..3 {
+        for j in 0..3 {
+            match input_box[3 * i + j] {
+                0 => print!("| "),
+                1 => print!("|O"),
+                -1 => print!("|X"),
+                _ => print!("|?")
+            } 
+        }
+        println!("|");
+    }
 }
