906a636652bacaf9317224f1e606ec5f80dd24f8
Peter Harliman Liem <peter.harliman@gmail.com>
2016-11-12 23:04:42 +0800

    Initial commit
    Signed-off-by: Peter Harliman Liem <peter.harliman@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+// Main function
+fn main() {
+    println!("Enter");
+    init_box();
+    println!("Exit");
+}
+
+// Initialize box
+fn init_box() {
+    println!("Initializing box");
+    let my_box = [0; 9];
+    println!("Finished initializing");
+}
